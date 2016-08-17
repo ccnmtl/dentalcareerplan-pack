583111f51cc47323e9a82f3604cdbcde2b5008e9
zarina <zarina@columbia.edu>
2016-08-17 18:41:51 -0400

    remove console.log mistake

--- src/careerplan.js
+++ src/careerplan.js
@@ -65,7 +65,6 @@ var BaseView = Backbone.View.extend({
         this.complete = true;
         this.render();
         this.trigger('complete', this);
-        console.log(this);
         jQuery('html, body').animate({ scrollTop: 0 }, 'slow');
     },
     markAnswers: function(evt) {
