b06239993d50d8c43454fe3fa2994f8d74a8920d
Susan Dreher <sdreher@gmail.com>
2018-04-30 20:26:36 -0400

    webpack4 upgrade

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ require('!file-loader?name=[name].[ext]!../static/index.html');
 require('./static.js');
 
 // load and apply css
-require('!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css');
-// eslint-disable-next-line security/detect-non-literal-require
-require('!style-loader!css-loader!bootstrap-arrow-buttons/dist/css/' +
-        'bootstrap-arrow-buttons.css');
-require('!style-loader!css-loader!../static/css/common.css');
-require('!style-loader!css-loader!../static/css/steps.css');
-require('!style-loader!css-loader!../static/css/careerplan.css');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-arrow-buttons/dist/css/bootstrap-arrow-buttons.css';
+import '../static/css/common.css';
+import '../static/css/steps.css';
+import '../static/css/careerplan.css';
 
-var jQuery = require('jquery');
-var module = require('./careerplan.js');
+import jQuery from 'jquery';
+import module from './careerplan.js';
 
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = decodeURIComponent(window.location.search.substring(1));
